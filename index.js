4ee532fcadc5bedca05cfb9a229ca2997aefc7f3
Julian Reyes <julianreyes4@gmail.com>
2023-10-27 08:13:10 +0900

    first commit on 10-27-2023

---
+++ index.js
@@ -0,0 +1,8 @@
+// alert("Welcome to my page.");    
+
+let contactInfoBox = document.getElementById("contact-info-box");
+
+contactInfoBox.addEventListener("click", () => {
+    alert("I got clicked");
+}
+)   
